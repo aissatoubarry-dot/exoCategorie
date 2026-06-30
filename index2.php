aa139f2c5e90c0b4c07f872534b74f99b6eecdce
aissatoubarry-dot <aissatoubarrysow2@gmail.com>
2026-06-30 20:26:57 +0000

    Initialisation du tableau

---
+++ index2.php
@@ -0,0 +1,87 @@
+<?php
+
+$categories = [
+
+    0 => [
+            "code" => "cat0",
+            "nom" => "categorieA",
+            "produits" => [
+                  0 => [
+                    "nom" => "produitA",
+                    "reference" => "refA",
+                    "prix" => 1500,
+                    "quantite" => 4
+                  ],
+                  1 => [
+                    "nom" => "produitB",
+                    "reference" => "refB",
+                    "prix" => 2500,
+                    "quantite" => 6
+                  ]
+            ]
+         ],
+
+    1 => [
+            "code" => "cat1",
+            "nom" => "categorieB",
+            "produits" => []
+         ]
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
